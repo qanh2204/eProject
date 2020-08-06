9360e80122c756962cb23a2b6f1248064b34f239
tncong92@gmail.com <tncong92@gmail.com>
2020-08-06 07:55:36 +0700

    Cong update 6/8

---
+++ assets/js/javascript.js
@@ -0,0 +1,12 @@
+// Counter
+$('.counter-count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 2000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
