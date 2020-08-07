2620147e924a8ec46f26a554aa40dcfa133cbf4f
tncong92@gmail.com <tncong92@gmail.com>
2020-08-07 10:57:29 +0700

    Cong 2 0708

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -10,3 +10,23 @@ $('.counter-count').each(function () {
         }
     });
 });
+// Image Profile
+$(document).ready(function() {
+
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.avatar').attr('src', e.target.result);
+            }
+    
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    
+
+    $(".file-upload").on('change', function(){
+        readURL(this);
+    });
+});
