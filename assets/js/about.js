5b083e7e777a89376df5ad00a3551c4aefa7e6b1
qanh2204@gmail.com <qanh2204@gmail.com>
2020-08-11 16:54:24 +0700

    about fixed

---
+++ assets/js/about.js
@@ -0,0 +1,7 @@
+/*------------------
+        Background Set
+    --------------------*/
+$('.set-bg').each(function() {
+    var bg = $(this).data('setbg');
+    $(this).css('background-image', 'url(' + bg + ')');
+});
